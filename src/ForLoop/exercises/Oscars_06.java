f90c43199c6fd6d0b2d9a9587c7798e1c01e24cf
I-Zafirov <ivoomail@gmail.com>
2021-10-06 09:58:25 +0300

    new

--- src/ForLoop/exercises/Oscars_06.java
+++ src/ForLoop/exercises/Oscars_06.java
@@ -2,25 +2,31 @@ package ForLoop.exercises;
 
 import java.util.Scanner;
 
-public class Oscars_06<sumPoints> {
+public class Oscars_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
         double academyPoints = Double.parseDouble(scanner.nextLine());
         int juryNum = Integer.parseInt(scanner.nextLine());
-        String juryName = scanner.nextLine();
-        double juryPoints = Double.parseDouble(scanner.nextLine());
+
         double sumPoints = 0.0;
 
-        for (int i = 0; i < juryNum; i++){
+        for (int i = 1; i <= juryNum; i++) {
+            String juryName = scanner.nextLine();
+            double juryPoints = Double.parseDouble(scanner.nextLine());
+
             int length = juryName.length();
-            sumPoints = academyPoints+(length*juryPoints/2);
+            sumPoints += (length * juryPoints / 2);
+
+            if ((sumPoints + academyPoints) > 1250.5) {
+                break;
+            }
         }
-        if(sumPoints>1250.5){
-            System.out.printf("Congratulations, %s got a nominee for leading role with %f!", name,sumPoints);
-        }
-        else {
-            System.out.printf("Sorry, %s you need %f more!",name, 1250.5-sumPoints);
+        sumPoints += academyPoints;
+        if (sumPoints > 1250.5) {
+            System.out.printf("%nCongratulations, %s got a nominee for leading role with %.1f!", name, sumPoints);
+        } else {
+            System.out.printf("Sorry, %s you need %.1f more!", name, 1250.5 - sumPoints);
         }
     }
 }

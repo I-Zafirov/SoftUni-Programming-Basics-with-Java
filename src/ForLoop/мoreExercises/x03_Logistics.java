776f1a614535f6aaa2a3963dde3d67ff057813b9
I-Zafirov <ivoomail@gmail.com>
2021-10-17 17:00:33 +0300

    Initial commit

---
+++ src/ForLoop/мoreExercises/x03_Logistics.java
@@ -0,0 +1,12 @@
+package ForLoop.мoreExercises;
+
+import java.util.Scanner;
+
+public class x03_Logistics {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+int loadsNumber = Integer.parseInt(scanner.nextLine());
+
+
+    }
+}

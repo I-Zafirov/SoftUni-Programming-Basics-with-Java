77700f3d7dd4c4b6e5f0d224cdd5d49a0636839d
I-Zafirov <ivoomail@gmail.com>
2021-10-26 10:45:30 +0300

    Initial commit

---
+++ src/Exam/Exam/x05_Everest_UnderDevelopment.java
@@ -0,0 +1,36 @@
+package Exam.Exam;
+
+import java.util.Scanner;
+
+public class x05_Everest_UnderDevelopment {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int goal = 3484;
+        int steps = 0;
+        int countYes = 0;
+        int countAll = 0;
+
+
+        while (goal >= steps) {
+            String input = scanner.nextLine();
+            countAll++;
+            if (input.equals("END")) {
+
+                break;
+            }
+            if (input.equals("Yes")) {
+                countYes++;
+            }
+
+            steps += Integer.parseInt(scanner.nextLine());
+        }
+        if (steps >= goal) {
+            System.out.printf("Goal reached for %d days!", countAll);
+        } else if (countYes < 5) {
+            System.out.println("Failed!");
+            System.out.println(steps + 5364);
+        }
+    }
+}

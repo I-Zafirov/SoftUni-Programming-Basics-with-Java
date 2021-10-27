ee81364ac8a1fe9357b2fb2b8aa1bf94688c7b9a
I-Zafirov <ivoomail@gmail.com>
2021-10-27 21:10:08 +0300

    Initial commit

--- src/Exam/x10And11March2018/PictureInTheWall.java
+++ src/Exam/x10And11March2018/PictureInTheWall.java
@@ -2,7 +2,7 @@ package Exam.x10And11March2018;
 
 import java.util.Scanner;
 
-public class PicInTheWall {
+public class PictureInTheWall {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
